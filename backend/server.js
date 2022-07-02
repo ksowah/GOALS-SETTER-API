2ba23599e9a0b54adb89c839a39bae5487a187ab
ksowah <sowahkelvin640@gmail.com>
2022-07-02 22:40:17 +0000

    refactor server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("server running");
+const express = require("express")
+const app = express()
+const {errorHundler} = require("./middlewear/errorMiddlewear")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 8000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use("/api/goals", require("./routes/goalRouts"))
+app.use(errorHundler)
+
+
+app.listen(PORT, () => console.log(`server running on prot ${PORT}...`))
