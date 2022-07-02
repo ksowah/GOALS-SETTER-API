cd6d64235968d968ffa1eec5d7d8d3fd887b883a
ksowah <sowahkelvin640@gmail.com>
2022-07-02 22:41:05 +0000

    clean routes

---
+++ backend/routes/goalRouts.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const {
+    getGoals, setGoal, updateGoal, deleteGoal
+} = require("../controller/goalController")
+
+router.route("/").get(getGoals).post(setGoal)
+
+router.route("/:id").put(updateGoal).delete(deleteGoal)
+
+module.exports = router
