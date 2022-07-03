636c8831ff992bc5b95499c35e44a099e8ef7164
ksowah <sowahkelvin640@gmail.com>
2022-07-03 00:23:51 +0000

    parse db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const colors = require("colors")
 const app = express()
 const {errorHundler} = require("./middlewear/errorMiddlewear")
+const connectDB = require("./config/db")
 const dotenv = require("dotenv").config()
 const PORT = process.env.PORT || 8000
+
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
