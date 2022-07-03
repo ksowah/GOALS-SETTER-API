ae86982fee8a6a31df6e812db6191a6923ed9446
ksowah <sowahkelvin640@gmail.com>
2022-07-03 00:24:06 +0000

    create db model

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Database connected ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+    
+}
+
+module.exports = connectDB
