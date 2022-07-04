11c0e1ddf095f3362174d708e95dda185cc7ba66
ksowah <sowahkelvin640@gmail.com>
2022-07-04 13:01:29 +0000

    add users route

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use("/api/goals", require("./routes/goalRouts"))
+app.use("/api/users", require("./routes/userRoutes"))
 app.use(errorHundler)
 
 
