4f0a1a84883ca33ae46bf6dcbe697a691c91e505
ksowah <sowahkelvin640@gmail.com>
2022-07-04 13:11:03 +0000

    add user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please add a name"]
+    },
+    email:{
+        type: String,
+        required: [true, "Please add a email"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, "Please add a password"]
+    },
+},{
+    timestamps: true
+}
+
+)
+
+module.exports = mongoose.model("User", userSchema)
+
+
+
